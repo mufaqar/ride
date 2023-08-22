264fb91333e4e4575bb9df3a1419b7cb0ace6bbd
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-08-22 14:17:19 +0500

    banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -6,7 +6,7 @@ import { BsFillPlayFill } from 'react-icons/bs'
 function Banner() {
     return (
         <section className='py-16'>
-            <Image src="/images/bg1.svg" alt='bg1.png' width={989} height={643} className='absolute top-0 left-[50%] max-w-[50%] md:block hidden' />
+            <Image src="/images/bg1.svg" alt='bg1.png' width={989} height={643} className='absolute sm:top-0 sm:max-w-[50%] max-w-[80%] left-auto right-0 top-[100%]' />
             <div className='container mx-auto px-4 grid md:grid-cols-2 grid-cols-1 gap-10 relative z-10'>
                 <div>
                     <h3 className='md:text-4xl text-2xl font-medium text-Cstm_gren orkneyBold'>
@@ -30,7 +30,7 @@ function Banner() {
                         </Link>
                     </div>
                 </div>
-                <div className='md:bg-none bg-[url("/images/bg1.svg")] bg-right bg-no-repeat bg-contain'>
+                <div className='md:bg-none .bg-[url("/images/bg1.svg")] bg-right bg-no-repeat bg-contain'>
                     <Image src="/images/1.png" alt='1.png' width={830} height={643} />
                 </div>
             </div>
